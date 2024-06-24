36c62b24398682c9031be12948112900b0e8dc8e
David Akunts <david@akunts.ru>
2024-06-24 22:02:19 +0400

    Post added

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Post;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(5),
+            'content' => fake()->text(300),
+            'user_id' => fake()->numberBetween(1, 3),
+        ];
+    }
+}
