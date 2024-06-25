b7ce11debaf0157a1c416d2bf8b3d9ea493427bf
Toxoma <daniil.veselov2012@yandex.ru>
2024-06-25 19:44:29 +0300

    bootstrap + base layout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,25 +6,25 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <title>@yield('page.title', config('app.name'))</title>
     <style>
         a {
             text-decoration: none;
         }
     </style>
+    @yield('head')
 </head>
 <body>
 <div class="d-flex flex-column justify-content-between min-vh-100">
-    @include('includes.header')
+{{--    @include('includes.header')--}}
 
     <main class="flex-grow-1 py-3">
         @yield('content')
     </main>
 
-    @include('includes.footer')
+{{--    @include('includes.footer')--}}
 </div>
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/js/bootstrap.min.js"></script>
 </body>
 </html>
